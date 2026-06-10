519cc7643983c14942b49d635d9cbee5016efe7b
Nguyen Duy Phuoc <duyphuoc105204@gmail.com>
2026-06-10 17:58:46 +0700

    Django Basic Exercise 4

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView
+from .views import BookViewSet
+
+book_list = BookViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+book_detail = BookViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    path('books/', BookListCreateView.as_view()),
-    path('books/<int:pk>/', BookDetailView.as_view()),
+    path('books/', book_list),
+    path('books/<int:pk>/', book_detail),
 ]
--- book/views.py
+++ book/views.py
@@ -1,12 +1,40 @@
-from rest_framework import generics
+from rest_framework import viewsets
+
 from .models import Book
 from .serializers import BookSerializer
+from .pagination import BookPagination
 
-class BookListCreateView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
+
+class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
+    pagination_class = BookPagination
 
+    def get_queryset(self):
+        queryset = Book.objects.all()
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+        price = self.request.query_params.get("price")
+        quantity = self.request.query_params.get("quantity")
+
+        if title:
+            queryset = queryset.filter(
+                title__icontains=title
+            )
+
+        if author:
+            queryset = queryset.filter(
+                author__icontains=author
+            )
+
+        if price:
+            queryset = queryset.filter(
+                price__gte=price
+            )
+
+        if quantity:
+            queryset = queryset.filter(
+                quantity__gte=quantity
+            )
+
+        return queryset
